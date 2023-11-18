8e68f6170a7de6c0acec320fbccae8e8723ca9c8
miyajimayui <ym423honya@yahoo.co.jp>
2023-11-18 18:23:19 +0900

    11/18 作業分

--- composables/index.ts
+++ composables/index.ts
@@ -1,9 +1,25 @@
-import { createApp } from "vue";
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
-//import App from "./App.vue";
+//11/18 勉強内容
 
-//const app = createApp(App);
+const greeting = "hello";
+console.log(greeting);
+//consoleの使い方
+//デバックする際に、コードで使用した変数の値を出力して正しい値が入っているかを確認するために使用。
 
-//app.use(ElementPlus);
-//app.mount("#app");
+//ブロックスコープ→{}で囲まれた範囲
+//letはスコープの中で宣言可。letは再宣言も可能だが、同じブロックスコープ内では不可。
+
+//型と変数名を同時に宣言
+const age : number = 23;
+if(age > 20) {
+    let name : string = "yui";
+    console.log(name);
+}
+else {
+    let name = 'ai';
+    console.log(name);
+}
+
+//nullやundefinedを型として定義出来る。「データがない」という状況を表すのに有用。
+const val1 : null = null;
+const val2 : undefined = undefined;
+
