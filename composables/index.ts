6b078f95e4973dcb3b1b4ac13714c1d890715642
miyajimayui <ym423honya@yahoo.co.jp>
2023-11-20 20:42:59 +0900

    11/20 勉強分

--- composables/index.ts
+++ composables/index.ts
@@ -1,7 +1,12 @@
-//11/18 勉強内容
+
+
+//▶︎▶︎11/18 勉強内容
 
 const greeting = "hello";
 console.log(greeting);
+const a = "ゆい";
+console.log(a);
+console.log(greeting + a);
 //consoleの使い方
 //デバックする際に、コードで使用した変数の値を出力して正しい値が入っているかを確認するために使用。
 
@@ -9,17 +14,49 @@ console.log(greeting);
 //letはスコープの中で宣言可。letは再宣言も可能だが、同じブロックスコープ内では不可。
 
 //型と変数名を同時に宣言
-const age : number = 23;
-if(age > 20) {
-    let name : string = "yui";
-    console.log(name);
-}
-else {
-    let name = 'ai';
-    console.log(name);
-}
+const age  = 23;
+console.log(age);
 
-//nullやundefinedを型として定義出来る。「データがない」という状況を表すのに有用。
-const val1 : null = null;
-const val2 : undefined = undefined;
+
+// if(age > 20) {
+//     let name : string = "yui";
+//     console.log(name);
+// }
+// else {
+//     let name = 'ai';
+//     console.log(name);
+// }
+
+// //nullやundefinedを型として定義出来る。「データがない」という状況を表すのに有用。
+// const val1 : null = null;
+// const val2 : undefined = undefined;
+
+// //▶︎▶︎11/20勉強内容
+// //クラスの宣言
+// class Dummy {
+//     dummyCd01 : number = 2
+//     dummyCd02 : number = 0
+//     dummyMei01 : string = ""
+//     dummyKna01? : string //?をつけるとオプショナルな変数となり、省略が可能。 
+//     readonly dummyName : string = "佑衣" 
+    
+// }
+
+// //クラスのnew
+// const dummy = new Dummy();
+// console.log(dummy.dummyCd01);
+// console.log(dummy.dummyName);
+
+// //ターミナルを使ってディレクトリの操作ができるようになりたい。
+// //mkdir - ファイルを作成
+// //rm - ファイルを削除　※できてない。
+
+
+// //TODO: デバック実行がうまくいっていない。ファイル構成がまだ完成していないのが原因？ではなさそう。型宣言のコンパイルが通っていない模様。
+
+
+
+
+
+
 
